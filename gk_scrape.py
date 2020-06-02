30d49294972d0c3120729b9a7aae00585b2ba56b
Anirudh Kachroo <anirudh.kac@yahoo.com>
2020-06-02 14:38:36 +0530

    search by keyword

--- gk_scrape.py
+++ gk_scrape.py
@@ -2,15 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 import html5lib
 
-res = requests.get("https://www.greaterkashmir.com/latest/page/1")
-##print(res.text)
+def match(text,keywords):
+    for keyword in keywords:
+        if keyword in text:
+            return True
+    
+    return False
 
-soup = BeautifulSoup(res.text,"html5lib")
-news_list  = soup.find_all("div",class_="m-article-first-large-listing__inner")
-news = news_list[0]
-link = news.find("a")
-time  = news.find(class_="entry-date published")
-#print(news.text)
-print(link.text) # Headline
-print(link['href']) #  Link to text
-print(time.text)
+base_url = "https://www.greaterkashmir.com/latest/page/"
+#keywords=["internet","2G","ban","snapped"]
+keywords=["covid","COVID","covid19","covid-19","COVID-19"]
+for i in range(1,10):
+    print("Scarping page : "+str(i))
+    res = requests.get(base_url+str(i))
+    ##print(res.text)
+
+    soup = BeautifulSoup(res.text,"html5lib")
+    #All divs containing news 
+    news_list  = soup.find_all("div",class_="m-article-first-large-listing__inner")
+
+    for news in news_list:
+        link = news.find("a")
+        time  = news.find(class_="entry-date published")
+        #print(news.text)
+        if match(str(link.text),keywords):
+            print(link.text) # Headline
+            #print(link['href']) #  Link to text
+            print(time.text)
